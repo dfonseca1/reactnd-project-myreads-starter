f24cebc2d9159566e0d287ea166b12d7014e2f56
Daniel Fonseca <dfonseca1dev@gmail.com>
2019-01-29 19:58:06 -0200

    Remove unused code

--- src/components/bookShelf.js
+++ src/components/bookShelf.js
@@ -44,15 +44,6 @@ class BookShelf extends React.Component {
                                 <div className="book">
                                     <div className="book-top">
                                         <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail}")` }}></div>
-                                        {/* <div className="book-shelf-changer">
-                                            <select>
-                                                <option value="move" disabled>Move to...</option>
-                                                <option value="currentlyReading">Currently Reading</option>
-                                                <option value="wantToRead">Want to Read</option>
-                                                <option value="read">Read</option>
-                                                <option value="none">None</option>
-                                            </select>
-                                        </div> */}
                                         <ShelfChanger book={book} changeShelfHandler={this.changeShelfHandler} />
                                     </div>
                                     <div className="book-title">{book.title}</div>
