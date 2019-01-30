d749897309828bdfb84994c183329f5e4ea19784
Daniel Fonseca <dfonseca1dev@gmail.com>
2019-01-29 22:09:48 -0200

    Changes id value in map context

--- src/components/bookShelf.js
+++ src/components/bookShelf.js
@@ -28,10 +28,10 @@ class BookShelf extends React.Component {
                     <ol className="books-grid">
 
                         {this.props.books.map((book) => (
-                            <li key={book.title}>
+                            <li key={book.id}>
                                 <div className="book">
                                     <div className="book-top">
-                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail}")` }}></div>
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks && book.imageLinks.smallThumbnail}")` }}></div>
                                         <ShelfChanger book={book} changeShelfHandler={this.changeShelfHandler} />
                                     </div>
                                     <div className="book-title">{book.title}</div>
