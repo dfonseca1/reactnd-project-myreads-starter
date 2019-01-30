f5fce4321a85c4fb9fa14968715038c66facca48
Daniel Fonseca <dfonseca1dev@gmail.com>
2019-01-29 23:33:51 -0200

    Remove state from shelfChanger component

--- src/components/shelfChanger.js
+++ src/components/shelfChanger.js
@@ -3,38 +3,29 @@ import React, { Component } from 'react';
 class ShelfChanger extends React.Component {
     constructor(props) {
         super(props);
+    }
 
-        this.state = {
-            book: props.book,
-            shelfOptions: [
+    handleShelfChange(e){
+        this.props.changeShelfHandler(this.props.book, e.target.value);
+    }
+
+    render() {
+        let currentShelf = this.props.book.shelf;
+        let shelfOptions = [
                 { shelfDescription: "Move to...", shelfValue: "move" },
                 { shelfDescription: "Currently Reading", shelfValue: "currentlyReading" },
                 { shelfDescription: "Want to Read", shelfValue: "wantToRead" },
                 { shelfDescription: "Read", shelfValue: "read" },
                 { shelfDescription: "None", shelfValue: "none" },
-            ]
-        }
-
+            ];
         
-
-
-    }
-
-    handleShelfChange(e){
-        this.props.changeShelfHandler(this.state.book, e.target.value);
-    }
-
-    render() {
-        let currentShelf = this.state.book.shelf;
-        console.log("currentShelf", currentShelf);
         return (
             <div className="book-shelf-changer">
                 <select onChange={(e) => this.handleShelfChange(e)} >
-                    {this.state.shelfOptions.map((shelf, index) => 
+                    {shelfOptions.map((shelf, index) => 
                         (
                             <option key={index} value={shelf.shelfValue} 
-                            disabled={ (currentShelf === undefined && shelf.shelfValue === 'none') || shelf.shelfValue === 'move' || shelf.shelfValue === currentShelf} >
-                            {shelf.shelfDescription}</option>
+                            disabled={ (currentShelf === undefined && shelf.shelfValue === 'none') || shelf.shelfValue === currentShelf} >{shelf.shelfDescription}</option>
                         )
                     )}
                 </select>
